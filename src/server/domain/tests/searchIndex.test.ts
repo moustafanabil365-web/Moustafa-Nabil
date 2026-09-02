466e5f7f9b1c20098d40c426a872176437ffa2a1
Mostafa Abass <Mostafa.Abass@almatar.com>
2026-09-02 14:53:08 +0300

    test(search): use official hotel chains dataset in search index test

--- src/server/domain/tests/searchIndex.test.ts
+++ src/server/domain/tests/searchIndex.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { buildAirlineIndexEntries, buildHotelIndexEntries, findAirlineByIataCode } from '../indexers';
 import { TEST_AIRLINES } from '../../../src/utils/airlineTestData';
-import { OFFICIAL_AIRLINES_DATABASE, OFFICIAL_AIRLINES_DATABASE as OFFICIAL_HOTELS } from '../../../src/utils/bookingUtils';
+import { OFFICIAL_AIRLINES_DATABASE, OFFICIAL_HOTEL_CHAINS_DATABASE } from '../../../src/utils/bookingUtils';
 
 export async function runSearchIndexTests() {
   console.log('searchIndexTests: starting');
@@ -15,8 +15,8 @@ export async function runSearchIndexTests() {
     assert.ok(found, 'emirates should be represented in index entries when present');
   }
 
-  // hotel index builder should accept hotel arrays (use OFFICIAL_AIRLINES_DATABASE wrongly named for a smoke test)
-  const hotels = buildHotelIndexEntries((OFFICIAL_HOTELS as any) as any[]);
+  // hotel index builder should accept hotel arrays
+  const hotels = buildHotelIndexEntries(OFFICIAL_HOTEL_CHAINS_DATABASE as any[]);
   assert.ok(Array.isArray(hotels));
 
   console.log('searchIndexTests: completed');
